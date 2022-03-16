a4269aa97f627431df772614caf0f57845cc4e81
linhpandaa <99894967+linhpandaa@users.noreply.github.com>
2022-03-17 02:12:18 +0300

    add source for issue #2
    Build command: g++ -std=c++11 lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -8,3 +8,23 @@ int maxEven(vector<int> v)
             ans = x;
     return ans;
 }
+
+int nMin(vector<int> v)
+{
+    if (v.size() == 0)
+        return 0;
+    
+    int vmin = v[0], count = 1;
+    for (int i = 1; i < v.size(); i++)
+    {
+        if (v[i] == vmin)
+            count++;
+        else if (v[i] < vmin)
+        {
+            vmin = v[i];
+            count = 1;
+        }
+    }
+
+    return count;
+}
